2215102afe64586daa14b4882644f7d161ac1d5e
nombre00 <leo.salgado@duocuc.cl>
2025-06-21 11:09:24 -0400

    Añadir SwaggerConfig.java a la configuración de la API, 62 tests

--- src/main/java/com/ventasWeb/cl/ventasWeb/config/SwaggerConfig.java
+++ src/main/java/com/ventasWeb/cl/ventasWeb/config/SwaggerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-
+// Configura la docuemtación.
 @Configuration
 public class SwaggerConfig {
 
