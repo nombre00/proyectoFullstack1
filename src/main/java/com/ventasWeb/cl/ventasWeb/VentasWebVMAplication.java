5d0a075d3ca4fb2dc79d2dbbf0f6de8ca5cba199
nombre00 <leo.salgado@duocuc.cl>
2025-06-23 19:57:17 -0400

    Agregamos una excepcion al assembler de carrito para manejar carritos sin cliente

--- src/main/java/com/ventasWeb/cl/ventasWeb/VentasWebVMAplication.java
+++ src/main/java/com/ventasWeb/cl/ventasWeb/VentasWebVMAplication.java
@@ -10,3 +10,4 @@ public class VentasWebVMAplication {
         SpringApplication.run(VentasWebVMAplication.class, args);
     }    
 }
+ 
--- src/main/java/com/ventasWeb/cl/ventasWeb/assemblers/CarritoRepresentationModelAssembler.java
+++ src/main/java/com/ventasWeb/cl/ventasWeb/assemblers/CarritoRepresentationModelAssembler.java
@@ -16,12 +16,21 @@ public class CarritoRepresentationModelAssembler implements RepresentationModelA
     @Override
     public EntityModel<Carrito> toModel(Carrito carrito) {
 
+        // Cuando el carrito no tiene cliente.
+        if (carrito.getCliente() == null){
+            return EntityModel.of(carrito,
+            // Linkeamos a si mismo.
+            linkTo(methodOn(CarritoControllerV2.class).buscarTodos()).withSelfRel());
+        }
+
+        // Cuando el carrito tiene cliente.
         return EntityModel.of(carrito,
         // Linkeamos a si mismo y al dueño del carrito.
         linkTo(methodOn(CarritoControllerV2.class).buscarPorRun(carrito.getCliente().getRun_cliente())).withSelfRel(),
         linkTo(methodOn(ClienteControllerV2.class).
         buscarPorRun(carrito.getCliente().getRun_cliente())).withRel("Cliente."),
-        linkTo(methodOn(VentaWebControllerV2.class).valorCarrito(carrito.getCliente().getRun_cliente())).withRel("Revisar costo del carrito.")
+        linkTo(methodOn(VentaWebControllerV2.class).
+        valorCarrito(carrito.getCliente().getRun_cliente())).withRel("Revisar costo del carrito.")
         );
     }
 }
